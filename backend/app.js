8aafe1ce2c52e4f8d1113000d815de1cf05ce077
Bharat Bhammar <bhartbhammar3336@gmail.com>
2023-03-15 16:08:59 +0530

    backend error resolved

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ import payment from './routes/paymentRoute.js'
 
 //Cors Options 
 const corsOptions = {
-    origin:h["https://bharatecommerce.onrender.com/", "https://bharatecommerce.onrender.com"],
+    origin:["https://bharatecommerce.onrender.com/", "https://bharatecommerce.onrender.com"],
     optionsSuccessStatus: 200,
     credentials: true,
  };
