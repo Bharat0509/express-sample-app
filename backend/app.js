5c2cf5952da908903841fed8a2dc8cd8a913f752
Bharat Bhammar <bhartbhammar3336@gmail.com>
2023-03-28 19:00:44 +0530

    bug fix -1

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ import payment from './routes/paymentRoute.js'
 //Cors Options
 const productionOrigin=[] 
 const corsOptions = {
-    origin:["http://127.0.0.1:4000","http://127.0.0.1:3000","http://localhost:4000","http://localhost:3000","https://bharatecommerce.onrender.com/", "https://bharatecommerce.onrender.com","https://bharatecom.onrender.com","https://bharatecom.onrender.com/"],
+    origin:["http://127.0.0.1:4000","http://127.0.0.1:3000","http://localhost:4000","http://localhost:3000","https://bharatecommerce.onrender.com/", "https://bharatecommerce.onrender.com","https://bharateccomerce.onrender.com/","https://bharateccomerce.onrender.com"],
     optionsSuccessStatus: 200,
     credentials: true,
  };
