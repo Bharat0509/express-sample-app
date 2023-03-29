3f4dddc8105fa2094ffbb26c13f8939eb9b5e030
Bharat Bhammar <bhartbhammar3336@gmail.com>
2023-03-29 14:18:05 +0530

    Cookie problem ig resulved

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -8,10 +8,7 @@ const sendToken = (user, statusCode, res) => {
     const options = {
     expires: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite:"None",
-// secure:true
-
-
+    secure:true
   }
   user = {...user._doc, token}
   return res.cookie('token', token, options).status(statusCode).json({
