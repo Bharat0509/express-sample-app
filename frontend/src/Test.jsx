aa96a16616710f8c9bd92d0e53b27c1f4ca62d13
Bharat Bhammar <bhartbhammar3336@gmail.com>
2023-03-29 13:20:49 +0530

    new build command added

--- frontend/src/Test.jsx
+++ frontend/src/Test.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react'
 
 const Test = () => {
     const testUrl = async () => {
-        return await axios.get('http://127.0.0.1:4000/api/v1/getCookie', { withCredentials: true })
+        return await axios.get('/api/v1/getCookie', { withCredentials: true })
     }
     useEffect(() => {
         testUrl();
